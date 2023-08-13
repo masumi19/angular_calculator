965035eacea3513a82b7eb096643d18a383a5fac
masumi19 <64879730+masumi19@users.noreply.github.com>
2023-08-13 11:27:36 +0900

    足し算（console.logにて）
    console.logにて数字と足し算の合計が出るようになった。

--- home.page.ts
+++ home.page.ts
@@ -7,6 +7,10 @@ import {IonButton, IonInput} from "@ionic/angular";
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  private calc: number;
+  private tmpCalc: number;
+  private beforeCalc: string = '';
+  private beforeSymbol: string;
 
   @ViewChild('total') public total: {
     el: IonInput
@@ -17,6 +21,24 @@ export class HomePage implements OnInit {
 　
 //記号の表示
   symbol(symbol: string) {
+    if (/^[0-9]*$/.test(symbol)) {
+      this.beforeCalc += symbol;
+    } else {
+      console.log(this.tmpCalc, this.beforeSymbol);
+      if (this.tmpCalc && this.beforeSymbol === '+') {
+        this.calc = this.tmpCalc + Number(this.beforeCalc);
+        this.beforeSymbol = '';
+        console.log(this.calc);
+      }
+
+      if (this.beforeCalc) {
+        this.tmpCalc = Number(this.beforeCalc);
+      }
+      this.beforeCalc = '';
+      console.log(symbol);
+      this.beforeSymbol = symbol;
+    }
+
     const display = this.total.el.value;
     this.total.el.value = display + symbol;
   }
@@ -29,11 +51,11 @@ export class HomePage implements OnInit {
   //合計の表示
   calculation() {
     const display = this.total.el.value;
-    const formula = Number(display);
-      const result = this.reckoning(formula);
-      this.total.el.value = result.toString();
     console.log(this.total.el.value);
-
+    const formula = Number(display);
+    const result = this.reckoning(formula);
+    this.total.el.value = result.toString();
+    console.log(this.total.el.value);
   }
   reckoning(display: number): number {
       const result = display;
